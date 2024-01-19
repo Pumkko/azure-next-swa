d9a66fd266acd0a4b3130cc5c33c37b1fdc50dcf
pumkko <c.coque@hotmail.fr>
2024-01-19 14:36:12 -0500

    try to redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    distDir: 'dist'
+    distDir: 'dist',
+    redirects: async () => {
+        return [
+            {
+                source: '/_next/*',
+                destination: '/dist/*'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
